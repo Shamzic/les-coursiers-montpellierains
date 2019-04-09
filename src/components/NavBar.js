9a0c3a2412f2e2808b55827058548e5f55abd646
Shamzic <shamery@free.fr>
2019-04-09 19:22:03 +0200

    fix css script bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,11 @@
 import React from 'react'
-// import './css/NavBar.css'
+import '../css/NavBar.css'
 
 function NavBar() {
   return (
     <div>
-      <nav>
-        <div class="nav-wrapper black">
-          <a href="#" class="brand-logo center red">Logo</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a href="sass.html">Commander</a></li>
-            <li><a href="badges.html">Nous contacter</a></li>
-            <li><a href="collapsible.html">Nos coursiers</a></li>
-          </ul>
-        </div>
-      </nav>
+      <h1>Les Coursiers Montpellierains</h1>
+      <p className="flow-text">flow text</p>
     </div>
   )
 }
