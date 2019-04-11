8c916c89ea62379fef6e3d1d6fd17ac2e54282e0
Shamzic <shamery@free.fr>
2019-04-11 19:25:24 +0200

    notifications

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import '../css/NavBar.css'
 
@@ -6,15 +7,18 @@ function NavBar() {
     <div>
       <nav className="nav-wrapper indigo">
       <div className="container">
-        <a href="#" class="brand-logo">Les Coursiers MTP</a>
-        <a href="#" class="sidenav-trigger" data-target="mobile-links">
+        <a href="#" className="brand-logo">Les Coursiers MTP</a>
+        <a href="#" className="sidenav-trigger" data-target="mobile-links">
           <i className="material-icons">menu</i>
         </a>
-        <ul class="right hide-on-med-and-down">
+        <ul className="right hide-on-med-and-down">
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
-          <li><a href="#">Login</a></li>
+          <li><a href="#" className="btn-floating indigo darken-4 z-depth-0">
+            <i className="material-icons">notifications</i>
+          </a></li>
+          <li><span className="badge white-text pink new ">5</span></li>
         </ul>
 
         <ul className="sidenav" id="mobile-links">
@@ -22,7 +26,7 @@ function NavBar() {
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-
+      
       </div>
       </nav>
     </div>
