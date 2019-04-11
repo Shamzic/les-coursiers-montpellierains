3bc6fc6427526a61b8286de86633687a4e48029d
shamzic <shamery@free.fr>
2019-04-11 18:50:47 +0200

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,27 @@ import '../css/NavBar.css'
 function NavBar() {
   return (
     <div>
-      <h1>Les Coursiers Montpellierains</h1>
-      <p className="flow-text">flow text</p>
+      <nav className="nav-wrapper indigo">
+      <div className="container">
+        <a href="#" class="brand-logo">Les Coursiers MTP</a>
+        <a href="#" class="sidenav-trigger" data-target="mobile-links">
+          <i className="material-icons">menu</i>
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Login</a></li>
+        </ul>
+
+        <ul className="sidenav" id="mobile-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+      </div>
+      </nav>
     </div>
   )
 }
