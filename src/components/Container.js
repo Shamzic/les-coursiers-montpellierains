87a96f471511d0459ab43f2c2bc845fa37813f9e
Shamzic <shamery@free.fr>
2020-03-19 15:31:40 +0100

    update col top container

--- src/components/Container.js
+++ src/components/Container.js
@@ -44,18 +44,16 @@ class Container extends React.Component {
     <div className="container content center">
       <h5 id="subtitle"> "Première coopérative Montpelliéraine de livraison à vélo"</h5>
       <div class="row">
-
-
-        <div class="col  s12 m12 l6">
+        <div class="col  s12 m12 l5">
           <div class="row">
             <div class="col s8 offset-s2">
               <a href="https://coursiersmontpellier.coopcycle.org" className="col s12 btn-large waves-effect waves-light cmd-button repas"><span className="text-repas">LIVRAISON DE REPAS</span><i className="btn material-icons right pulse">send</i> </a>
             </div>
           </div>
-          <p> Faites vous livrer votre repas chaque jour entre <b>11h30</b> et <b>14h30</b> et entre <b>18h</b> et <b>23h</b> via notre plateforme <b>coopcycle</b> disponible en <a href="https://coursiersmontpellier.coopcycle.org">web</a> et sur <a href="https://play.google.com/store/apps/details?id=fr.coopcycle">android</a>.</p>
+          <p> Faites vous livrer votre repas chaque jour entre <b>11h30</b> et <b>14h30</b> et entre <b>18h</b> et <b>23h</b> via notre plateforme <b>coopcycle</b> disponible en <a href="https://coursiersmontpellier.coopcycle.org">web</a>, sur <a href="https://play.google.com/store/apps/details?id=fr.coopcycle">android</a> et sur <a href="https://apps.apple.com/us/app/coopcycle/id1324884530?l=fr">iphone</a>.</p>
           <br/>
         </div>
-        <div class="col  s12 m12 l6">
+        <div class="col  s12 m12 l5 offset-l1">
           <div class="row">
             <div class="col s8 offset-s2">
               <a href="#commander_un_coursier">
@@ -69,7 +67,9 @@ class Container extends React.Component {
           <p>Livraison à <b>toute heure de la journée</b> de plis, colis, réassorts et tout autre type de livraison possible en vélo cargo ou vélo classique via un <b>formulaire de course unique </b> ou par <b>contact immédiat</b>.</p>
         </div>
       </div>
-      <div className="col s6 offset-s3 btn-small waves-effect waves-light cmd-button" onClick={this.contactShow.bind(this)}>{this.state.contact}</div>
+
+      <div class="row">
+      <div className="col s3 offset-s4 btn-small waves-effect waves-light cmd-button" onClick={this.contactShow.bind(this)}>{this.state.contact}</div>
         <div  className="col s6 offset-s3" hidden={this.state.contactHidden}>
             <span className="contact-numbers">
               <p><b>Paul Ferreira - 06.30.72.94.41</b></p>
@@ -77,6 +77,7 @@ class Container extends React.Component {
               <p><b>Vincent Robillard - 06.37.73.68.43</b></p>
             </span>
         </div>
+    </div>
     </div>
     </div>
   )
